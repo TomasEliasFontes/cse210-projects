f329b9ac1efad59d63a9d0fa67b1c74c9484a672
tomaseliasfontes <tomaseliasfontes@gmail.com>
2023-06-07 17:49:45 -0300

    Learning Activity - C# Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,55 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade: ");
+        string grades = Console.ReadLine();
+        int number = int.Parse(grades);
+
+        string letter = "";
+        string sign ="";
+        if (number >= 90)
+        {
+            letter = "A";
+        }
+        else if (number >= 80)
+        {
+            letter = "B";
+        }
+        else if (number >= 70)
+        {
+            letter = "C";
+        }
+        else if (number >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        if ( (number >= 87 || number >= 77 || number >= 67) && letter != "A")
+        {
+            sign = "+";
+        }
+        else if ((number < 63 || number < 73 || number < 83 ) && letter != "F")
+        {
+            sign = "-";
+        }
+        else 
+        {
+            sign = "";
+        }
+        Console.WriteLine($"Your grade is: {letter}{sign}");
+        
+        if (number >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
+
 }
